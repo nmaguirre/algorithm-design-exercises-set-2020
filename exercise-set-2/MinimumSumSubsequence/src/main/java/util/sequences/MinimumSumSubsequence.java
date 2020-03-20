9f4389f1a2eda07687d37250ada8fd1feceef18c
Gastón Scilingo <gaston@MacBook-Pro-de-Gaston.local>
2020-03-20 02:45:44 -0300

    Add minimum sum subsequence

---
+++ exercise-set-2/MinimumSumSubsequence/src/main/java/util/sequences/MinimumSumSubsequence.java
@@ -0,0 +1,24 @@
+package util.sequences;
+
+import java.util.Arrays;
+
+/**
+ * Computeds minimum sum subsequence of Integer sequences
+ * 
+ * @author scilingo
+ */
+
+public class MinimumSumSubsequence{
+
+	/**
+	* Computes a minimum sum subsequence by divide and conquer strategy.
+	* @param sequence is an Integer sequence.
+	* @return return a tuple containing,  value of the sum, the begin index
+	* of subsequence and the end of the subsequence
+	*/
+	public static Tuple <Integer,Integer,Integer> minimumSumSubsequence (Integer[] sequence){
+		throw new UnsupportedOperationException("method not yet implemented");
+	}
+
+
+}
