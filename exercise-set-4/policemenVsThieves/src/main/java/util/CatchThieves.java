3d6675982d1eb6f90ddc96c1a841a1ff930416f5
sonia <spermigiani@dc.exa.unrc.edu.ar>
2020-04-20 18:18:05 -0300

    add template of policemenVsThieves (exercise set 4)

---
+++ exercise-set-4/policemenVsThieves/src/main/java/util/CatchThieves.java
@@ -0,0 +1,23 @@
+package util;
+
+/**
+ * Greedy Policemen catch thieves.
+ *
+ * @author sonia
+ *
+ */
+
+public final class CatchThieves {
+   
+  
+  /**
+   * Returns the maximum number of thieves that can be caught.
+   * Each policeman can catch only one thief which is at most k away from him.
+   * @param seqTP is the sequence of policemen and thieves, p or t.
+   * @param distanceK represent the units away from the policemen to catch a thieve.
+   * @return the maximum number of thieves that can be caught.
+   */
+  public final int maxCatch(final char []seqTP, final int distanceK) {
+    throw new UnsupportedOperationException("method not yet implemented");
+  }  
+}   
